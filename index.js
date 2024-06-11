8baddd5117d7a51748bbddd6b8092c82e929e094
Grishkevich <kdarua4@gmail.com>
2024-06-11 15:42:30 +0300

    DemoVersion

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function show(){
+  document.getElementById("modal-overlay").style.display = "block";
+}
+function hide() {
+  document.getElementById("modal-overlay").style.display = "none";
+
+}
